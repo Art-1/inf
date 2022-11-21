cec721aa077bad60b9deacc4a32e71803990bc77
Art-1 <51963238+Art-1@users.noreply.github.com>
2022-11-21 16:27:23 +0500

    Add files via upload

--- задание3.c
+++ задание3.c
@@ -2,26 +2,37 @@
 #include <stdlib.h>
 
 
-float a, b, c, x_start = 30, x_end = 100.0, dx = 3, x, f, i;
+float a, b, c, dx = 3, x, f, i;
+int x_start = 30, x_end = 100;
 
 int main() {
-
-	//srand(time(NULL));
-	//for (i = 0; i <= 200; i++) {
-		//a = rand()%100/10.0;
-		a = 3.8;
-		//b = rand()%100/10.0;
-		b = 2.3;
-		//c = rand()%100/10.0;
-		c = 4.2;
+	
+		printf("x_start: \n");
+		scanf("%d", &x_start);
+		printf("x_end: \n");
+		scanf("%d", &x_end);
+		printf("dx: \n");
+		scanf("%f", &dx);
+		
+		printf("a: \n");
+		scanf("%f", &a);
+		printf("b: \n");
+		scanf("%f", &b);
+		printf("c: \n");
+		scanf("%f", &c);
+		
+		// INT: 0 2.5 5.321
+		// FLOAT: 3.04 23.1 0.9321
+		
+		
 		int A = (int) a;
 		int B = (int) a;
 		int C = (int) a;
-
+		
 		printf("a b c:\n");
 		printf("%f %f %f \n\n", a, b, c);
 		printf("  x  |  f(x) \n");
-
+		
 		for (x = x_start; x <= x_end; x ++) {
 			if (c < 0 && b != 0) {
 				f = a*x*x + b*b*x;
@@ -30,49 +41,11 @@ int main() {
 			} else {
 				f = x/c;
 			}
-
-			if (((a && b) || (a && c)) == 0)  {
-				printf("%f  |  %d INT\n", x, (int)f);
+			
+			if (((A & B) | (A & C)) == 0)  {
+				printf("%f  |  %d\n", x, (int)f);
 			} else {
 				printf("%f  |  %f\n", x, f);
 			}
-		//}
 	}
-	return 0;
 }
-
-#include <stdio.h>
-#include <stdlib.h>
-
-float a, b, c, dx = 3, f, i;
-int x_start = 30, x_end = 100;
-
-int funct(a, b, c) {
-    int k;
-
-    for (int x = x_start; x <= x_end; x +dx) {
-        if (c < 0 & b != 0) {
-            f = a*x*x + b*b*x;
-        } else if (c > 0 & b == 0) {
-            f = (x+a)/(x+c);
-        } else {
-            f = x/c;
-        }
-
-        printf("%d    |   %d\n", x, (int)f);
-
-
-    }
-}
-
-int main() {
-    printf("a: \n");
-    scanf("%f", &a);
-    printf("b: \n");
-    scanf("%f", &b);
-    printf("c: \n");
-    scanf("%f", &c);
-
-    funct(a, b, c);
-}
-
