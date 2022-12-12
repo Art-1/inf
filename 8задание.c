149d52933ab960403dd8ddc9425d3c9284a7aadd
Art-1 <51963238+Art-1@users.noreply.github.com>
2022-12-12 18:09:48 +0500

    Add files via upload

---
+++ 8задание.c
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main() {
+    char *str;
+    char *astr;
+    char str_r[200] = "a arks as ksk ak safsaaf sa fsa";
+    str = strtok(str_r, " ");
+
+    //str = (char*) malloc(strlen(str));
+
+    int crr_int = 107;
+    while (str != NULL)
+   {
+		astr = strchr(str, crr_int);
+    	if (astr != NULL) printf ("%s\n",str);
+    	str = strtok (NULL, " ");
+   }
+}
