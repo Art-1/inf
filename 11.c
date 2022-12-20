768771fb27e16134d8bb367c5b0632bc3067af2b
Art-1 <51963238+Art-1@users.noreply.github.com>
2022-12-20 13:39:36 +0500

    22

--- 11.c
+++ 11.c
@@ -1,27 +1,22 @@
 #include <string.h>
 #include <stdio.h>
 
-int count_sum(int number, int count_n);
-
-void main() {
-    int num, c_n;
-	scanf("%d", &num);
-	scanf("%d", &c_n);
-	count_sum(num, c_n);
-}
-
-int count_sum(number, count_n) {
+int count_sum(int number) {
     int count, chr, s = 0;
 	while (number != 0) {
 		chr = number % 10;
 		count ++;
 		if (count % 2 != 0) s += chr;
 		number /= 10;
-		if (count >= count_n) {
-            printf("Limit\n");
-            break;
-		}
 	}
 	printf("count: %d\n sum: %d\n", count, s);
 
 }
+
+void main() {
+    int num;
+	scanf("%d", &num);
+	count_sum(num);
+}
+
+
