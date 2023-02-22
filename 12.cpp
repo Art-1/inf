6ce2b2004718f0cd752ad15b921e3c2214b74339
Art-1 <51963238+Art-1@users.noreply.github.com>
2023-02-22 11:25:50 +0500

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -1,27 +1,64 @@
-#include <string>
 #include <iostream>
-#include <fstream>
-#include <cstring>
+#include <string>
 
 using namespace std;
 
-struct dateStruct {
-        int age;
-        string male;
-        string education;
-        string second_group_ans;
-    };
+struct Person
+{
+	int age;
+	string male;
+	string education;
+	string answer;
+};
 
-int main(){
-    setlocale(LC_ALL, "rus");
-    string s;
-    dateStruct newPerson;
+int maleCount_40   = 0;
+int femaleCount_39 = 0;
+int maleCount_25   = 0;
 
-    ofstream text_file;
-    text_file.open("D:\\Desktop\\inf-main\\bd.txt");
+void getData(Person person) {
+	int age_e = (int)person.age;
 
-    for (text_file; !text_file.eof(); text_file) {
-        text_file.write("ok", strlen("ok"));
-    }
+	if (person.male != "m" || person.male != "w") {
+		cout << "Error male " << person.male << " not valid\n";
+		exit;
+	}
+	else {
+		if (person.male == "M") {
+			if (age_e >= 40 and person.education == "university" and person.answer == "y")
+				maleCount_40 += 1;
+			if (age_e <= 25 and person.education == "school" and person.answer == "y")
+				maleCount_25 += 1;
+		}
 
+		if (age_e <= 30 and person.education == "middle" and person.answer == "n" and person.male == "W") {
+			cout << "ok";
+		}
+	}
+		
+	
+}
+
+void main(){
+	//Person person1{18, "u", "school", "y"};
+	//getData(person1);
+	string breaked = "";
+
+	while (1) {
+		cout << "Break? (y/n)\n";
+		cin >> breaked;
+		if (breaked == "y") break;
+
+		Person person;
+
+		cout << "Age (int): ";
+		cin >> person.age;
+		cout << "Male (m/w): ";
+		cin >> person.male;
+		cout << "Education (university/middle/school): ";
+		cin >> person.education;
+		cout << "Answer (y/n): ";
+		cin >> person.answer;
+
+		getData(person);
+	}
 }
