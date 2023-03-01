8a86202750767dcbbce5cf865c7a3f36bec978d9
Art-1 <51963238+Art-1@users.noreply.github.com>
2023-03-01 11:22:50 +0500

    Update 18(1)задание.c

--- 18(1)задание.c
+++ 18(1)задание.c
@@ -1,27 +1,22 @@
-#include <stdio.h>
-#include <math.h>
+#include <iostream>
+#include <conio.h>
 
-int NOD(int a, int b){
-    while (a != b){
-        if (a > b){
-            a -= b;
-        } else {
-            b -= a;
-        }
-    }
-    return a;
+using namespace std;
+
+int gcd(int x, int y)
+{
+    return y ? gcd(y, x % y) : x;
 }
 
-int main(){
-    int a, b;
-    printf("Enter a and b: \n");
-    scanf("%d %d", &a, &b);
-    
-    if (NOD(a, b) == 1) {
-    	printf("Yes");
-	} else {
-		printf("No");
-	}
-    
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int m, n;
+    cin >> m >> n;
+    if (gcd(m, n) == 1)
+        cout << "Взаимно простые";
+    else
+        cout << "Не взаимно простые";
+
     return 0;
 }
