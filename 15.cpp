1fbc5bc4ab656fd8e0e43f59586531a40bf94a5f
Art-1 <51963238+Art-1@users.noreply.github.com>
2023-04-09 19:14:40 +0500

    Add files via upload

--- 15.cpp
+++ 15.cpp
@@ -10,49 +10,39 @@ int gcn(int x) {
 }
 
 int main() {
-
-  int n;
-  cout << "Length: ";
-  cin >> n;
-
-  int arr[n];
-  for (int i = 0; i < n; i++) {
-    arr[i] = i;
-  }
-  
-  cout << "main array" << endl;
-  for (int i = 0;i < n; i ++) cout << arr[i] << " ";
-  cout << endl;
-  
-  int prime_arr[n/2 + 1]; 
-  int other_arr[n/2 + 1]; 
-  int prime_count = 0; 
-  int other_count = 0; 
-  
-    for (int i = 0; i < n; i++) {
-    if (gcn(i)) {
-      prime_arr[prime_count] = arr[i];
-      prime_count++;
-    } else {
-      other_arr[other_count] = arr[i];
-      other_count++;
-    }
-  }
-
-  cout << endl;
-  cout << "2 array " << endl;
-  for (int i = 0; i < prime_count; i++) {
-    cout << prime_arr[i] << " ";
-  }
-  cout << endl;
-  cout << endl;
-
-  cout << "3 array ";
-  cout << endl;
-  for (int i = 0; i < other_count; i++) {
-    cout << other_arr[i] << " ";
-  }
-  cout << endl;
-
-  return 0;
+	/*
+	for (int i = 0; i < 20; i++) {
+		cout << gcn(i) << " " << i << "|";
+	}
+	*/
+	
+	int n;
+	cin >> n;
+	int a[n];
+	int a1[n], a2[n];
+	int count1 = 0, count2 = 0;
+	
+	cout << "Main array:\n";
+	for (int i = 0; i < n; i++ ) {
+		a[i] = i;
+		cout << a[i] << " ";
+	}
+	cout <<endl;
+	for (int i = 0; i < n; i++ ) {
+		if (gcn(i) == 1) {
+			a1[count1] = a[i];
+			count1++;
+		}
+		else {
+			a2[count2] = a[i];
+			count2++;
+		}
+	}
+	cout <<endl;
+	cout << "2 array:\n";
+	for (int i = 0; i < count1; i++) cout << a1[i] << " ";
+	cout <<endl;
+	cout << "3 array:\n";
+	for (int i = 0; i < count2; i++) cout << a2[i] << " ";
+	
 }
