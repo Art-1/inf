3fcc32a886e544295e23c6ab2c9d58629d5bbaf1
Art-1 <51963238+Art-1@users.noreply.github.com>
2023-04-26 09:39:05 +0500

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "cstring"
+using namespace std;
+ 
+const int N = 4;
+string names[4]  {"First name: \n", "Surname: \n", "Zodiak: \n", "Date of birth: \n"};
+ 
+
+struct Znak
+{
+    static const int SZ = 256;
+    char FirstName[SZ];
+    char LastName[SZ];
+    char Zodiak[SZ];
+    int DR[3];
+}  A[N];
+
+void get_value(int i) {
+	cout << "\n\n Written: \n";
+	cout << names[0];
+	cout << A[i].FirstName << endl;
+	cout << names[1];
+	cout << A[i].LastName << endl;
+	cout << names[2];
+	cout << A[i].Zodiak << endl;
+	cout << names[3];
+	for (int j = 0; j < 3; j++) {
+		cout << A[i].DR[j] << ".";
+	}
+	cout << "\n\n";
+}
+ 
+
+void set_value() {
+	for (int i = 0; i < N; i++) {
+		cout << names[0];
+		cin >> A[i].FirstName;
+		cout << names[1];
+		cin >> A[i].LastName;
+		cout << names[2];
+		cin >> A[i].Zodiak;
+		cout << names[3];
+		for (int j = 0; j < 3; j++) cin >> A[i].DR[j];
+		
+		get_value(i);
+		
+	}
+}
+ 
+int main()
+{
+	set_value();
+	
+	char zd;
+	cout << names[2];
+	cin >> zd;
+	int count = 0;
+	
+	
+	cout << "\n\n Result: \n";
+	for (int i = 0; i < N; i++) {
+		if (A[i].Zodiak[0] == zd) get_value(i);
+	}
+	
+	
+	
+	
+    return 0;
+}
