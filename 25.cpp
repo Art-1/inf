9b75f57534b65b36288f9d0cbe4273363fd76f97
Art-1 <51963238+Art-1@users.noreply.github.com>
2023-05-14 00:06:45 +0500

    Add files via upload

--- 25.cpp
+++ 25.cpp
@@ -1,38 +1,49 @@
-#include <stdio.h>
-#define NUM  5
- 
-typedef struct {
-    int x, y;
-} point;
- 
-int distance(const point* a, const point* b){
-    int dx = a->x - b->x;
-    int dy = a->y - b->y;
-    return dx*dx + dy*dy;
-}
- 
-int main(void){
-    int    i, j, m, d;
-    point* p1, *p2;
- 
-    point ds[NUM] = {
-        {50,145}, {10,145}, {110,112}, {117,19}, {130,120}
-    };
- 
-    p1 = p2 = &ds[0];
-    m  = 0;
-    for(i = 0; i < NUM; ++i){
-        for(j = i + 1; j < NUM; ++j){
-            if((d = distance(&ds[i], &ds[j])) > m){
-                m  = d;
-                p1 = &ds[i];
-                p2 = &ds[j];
-            }
+#include <iostream>
+#include <cstdlib> 
+#include <ctime> 
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+
+    int n;
+    const int m = 3;
+    int d;
+    int d_max = 0;
+    
+    cout << "Enter n: \n";
+    cin >> n;
+
+    int arr[n][m];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) { 
+            arr[i][j] = rand() % 10 + 1; 
         }
     }
- 
-    if(p1 != p2)
-        printf("%d,%d <-> %d,%d\n", p1->x, p1->y, p2->x, p2->y);
-    getchar();
+
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    for (int i = 0; i < n-1; i++) {
+    	for (int j = 0; j < i+1; j++) {
+    		int a1 = (arr[i][0] - arr[i+1][0]);
+    		int a2 = (arr[i][1] - arr[i+1][1]);
+    		int a3 = (arr[i][2] - arr[i+1][2]);
+    		d = sqrt(a1*a1 + a2*a2 + a3*a3);
+    		cout << "\n" << a1*a1 << " " << a2*a2 << " " << a3*a3 << " " << d << "\n";
+    		
+    		if (d > d_max) d_max = d;
+		}
+	}
+	cout << "Max lenght: " << d_max;
+
     return 0;
 }
