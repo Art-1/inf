8b718daccc526c6517fc118e5a3d0fdf6efeb495
Art-1 <51963238+Art-1@users.noreply.github.com>
2023-05-17 11:08:41 +0500

    Update 25.cpp

--- 25.cpp
+++ 25.cpp
@@ -9,18 +9,21 @@ int main() {
     srand(time(NULL));
 
     int n;
-    const int m = 3;
+    const int m = 2;
     int d;
     int d_max = 0;
     
     cout << "Enter n: \n";
     cin >> n;
+    if (n < 2) return 0;
 
     int arr[n][m];
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) { 
-            arr[i][j] = rand() % 10 + 1; 
+            int st = 0;
+            cin >> st;
+            arr[i][j] = st; 
         }
     }
 
@@ -36,9 +39,9 @@ int main() {
     	for (int j = 0; j < i+1; j++) {
     		int a1 = (arr[i][0] - arr[i+1][0]);
     		int a2 = (arr[i][1] - arr[i+1][1]);
-    		int a3 = (arr[i][2] - arr[i+1][2]);
-    		d = sqrt(a1*a1 + a2*a2 + a3*a3);
-    		cout << "\n" << a1*a1 << " " << a2*a2 << " " << a3*a3 << " " << d << "\n";
+    	//	int a3 = (arr[i][2] - arr[i+1][2]);
+    		d = sqrt(a1*a1 + a2*a2);
+    		//cout << "\n" << a1*a1 << " " << a2*a2 << " " << a3*a3 << " " << d << "\n";
     		
     		if (d > d_max) d_max = d;
 		}
