f941ff75d2b732d348ace58dbb80b6893902b8ca
Art-1 <51963238+Art-1@users.noreply.github.com>
2023-05-17 10:34:44 +0500

    Update 13.cpp

--- 13.cpp
+++ 13.cpp
@@ -1,69 +1,85 @@
+
+
+
+
+
+
+
+
+
+
+
 #include <iostream>
-#include "cstring"
-using namespace std;
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-const int N = 3;
-string names[4]{ "First name: \n", "Surname: \n", "Zodiak: \n", "Date of birth: \n" };
+// Структура для хранения информации о человеке
+struct ZNAK {
+    std::string surname;
+    std::string name;
+    std::string zodiac_sign;
+    std::string birth_date;
+    
+    bool operator<(const ZNAK& other) const {
+        return birth_date < other.birth_date;
+    }
+};
 
+// Функция для записи информации о человеке в файл
+void write_data(const ZNAK& person) {
+    std::ofstream file("data.txt", std::ios::app);
+    if (file.is_open()) {
+        file << person.surname << " " << person.name << " "
+             << person.zodiac_sign << " " << person.birth_date << std::endl;
+        file.close();
+        std::cout << "Данные успешно записаны в файл" << std::endl;
+    } else {
+        std::cout << "Ошибка открытия файла" << std::endl;
+    }
+}
 
-struct Znak
-{
-	static const int SZ = 256;
-	char FirstName[SZ];
-	char LastName[SZ];
-	char Zodiak[SZ];
-	int DR[3];
-}  A[N];
+// Функция для чтения информации о людях с определенным знаком зодиака из файла
+std::vector<ZNAK> read_data(const std::string& zodiac_sign) {
+    std::ifstream file("data.txt");
+    std::vector<ZNAK> persons;
+    if (file.is_open()) {
+        ZNAK person;
+        while (file >> person.surname >> person.name >> person.zodiac_sign >> person.birth_date) {
+            if (person.zodiac_sign == zodiac_sign) {
+                persons.push_back(person);
+            }
+        }
+        file.close();
+        std::sort(persons.begin(), persons.end());
+    } else {
+        std::cout << "Ошибка открытия файла" << std::endl;
+    }
+    return persons;
+}
 
-void get_value(int i) {
-	cout << "\n\nWritten: \n";
-	cout << names[0];
-	cout << A[i].FirstName << endl;
-	cout << names[1];
-	cout << A[i].LastName << endl;
-	cout << names[2];
-	cout << A[i].Zodiak << endl;
-	cout << names[3];
-	for (int j = 0; j < 3; j++) {
-		cout << A[i].DR[j] << ".";
-	}
-	cout << "\n\n";
+int main() {
+    // Записываем информацию о людях в файл
+    ZNAK person1 = {"Иванов", "Иван", "Овен", "01.04.1990"};
+    ZNAK person2 = {"Петров", "Петр", "Лев", "15.08.1985"};
+    ZNAK person3 = {"Сидорова", "Мария", "Овен", "10.03.1995"};
+    write_data(person1);
+    write_data(person2);
+    write_data(person3);
+
+    // Считываем информацию о людях с знаком Овен из файла и сортируем по датам рождения
+    std::vector<ZNAK> persons = read_data("Овен");
+    if (persons.empty()) {
+        std::cout << "Людей с знаком Овен не найдено" << std::endl;
+    } else {
+        std::cout << "Найдены люди с знаком Овен, отсортированные по датам рождения:" << std::endl;
+        for (const auto& person : persons) {
+            std::cout << person.surname << " " << person.name << ", дата рождения: " << person.birth_date << std::endl;
+        }
+    }
+
+    return 0;
 }
 
 
-void set_value() {
-	for (int i = 0; i < N; i++) {
-		cout << names[0];
-		cin >> A[i].FirstName;
-		cout << names[1];
-		cin >> A[i].LastName;
-		cout << names[2];
-		cin >> A[i].Zodiak;
-		cout << names[3];
-		for (int j = 0; j < 3; j++) cin >> A[i].DR[j];
-
-		get_value(i);
-
-	}
-}
-
-int main()
-{
-	set_value();
-
-	char zd;
-	cout << names[2];
-	cin >> zd;
-	int count = 0;
-
-
-	cout << "\n\nResult: \n";
-	for (int i = 0; i < N; i++) {
-		if (A[i].Zodiak[0] == zd) get_value(i);
-	}
-
-
-
-
-	return 0;
-}
