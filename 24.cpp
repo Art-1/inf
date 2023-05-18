072cd0d198af84a51bb34a5c9985ff7245c6fa8c
Art-1 <51963238+Art-1@users.noreply.github.com>
2023-05-18 23:24:01 +0500

    Update 24.cpp

--- 24.cpp
+++ 24.cpp
@@ -1,52 +1,47 @@
 #include <iostream>
-#include <cstdlib> 
-#include <ctime> 
-#include <cmath>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-    srand(time(NULL));
-
-    int n;
-    const int m = 2;
-    float d;
-    float d_max = 0;
+    string path = "text.txt";
+    string max_str;
     
-    cout << "Enter n: \n";
-    cin >> n;
-    if (n < 2) return 0;
-
-    float arr[n][m];
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) { 
-            float st = 0;
-            cin >> st;
-            arr[i][j] = st; 
-        }
-    }
-
+    ifstream fin;
+    fin.open(path);
     
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
+    if (!fin.is_open()) cout << "File error";
     
-    for (int i = 0; i < n-1; i++) {
-    	for (int j = 0; j < i+1; j++) {
-    		float a1 = (arr[i][0] - arr[i+1][0]);
-    		float a2 = (arr[i][1] - arr[i+1][1]);
-    	//	int a3 = (arr[i][2] - arr[i+1][2]);
-    		d = sqrt(a1*a1 + a2*a2);
-    		//cout << "\n" << a1*a1 << " " << a2*a2 << " " << a3*a3 << " " << d << "\n";
-    		
-    		if (d > d_max) d_max = d;
+    else {
+    	string str;
+    	
+    	while (!fin.eof()) {
+    		fin >> str;
+    		if (str.length() > max_str.length()) max_str = str;
 		}
+		cout << max_str << endl;
 	}
-	cout << "Max lenght: " << d_max;
-
+	fin.close();
+    fin.open(path);
+    
+    if (!fin.is_open()) cout << "File error";
+    
+    else {
+    	string str;
+    	int max_count = 0, count = 0;
+    	
+    	while (!fin.eof()) {
+    		fin >> str;
+    		if (str == max_str) {
+    			count++;
+    			max_count = count;
+			}
+		}
+		cout << max_count;
+	}
+	fin.close();
+	
+    
     return 0;
 }
