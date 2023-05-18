6849a27122ad8fed56f3cc1b01a79cd381768699
Art-1 <51963238+Art-1@users.noreply.github.com>
2023-05-18 23:36:40 +0500

    Update 22n.cpp

--- 22n.cpp
+++ 22n.cpp
@@ -5,50 +5,45 @@ using namespace std;
 
 int main() {
     int N, m;
-    cout << "Enter the number of elements to write: ";
+    cout << "N: ";
     cin >> N;
 
-    // Open file for writing
     ofstream fout("data.txt");
 
-    // Write N random numbers to file
     for (int i = 0; i < N; i++) {
         int num = rand() % 100;
         fout << num << " ";
     }
 
-    // Close file
     fout.close();
 
-    // Open file for reading
     ifstream fin("data.txt");
 
-    // Open file for writing
     ofstream fout2("data2.txt");
 
-    // Read numbers from first file and write to second file if they are divisible by m
-    cout << "Enter a number to check divisibility: ";
+    cout << "number to check divisibility: ";
     cin >> m;
+    cout << "data.txt: ";
     int num;
     while (fin >> num) {
+        cout << num << " ";
         if (num % m == 0) {
             fout2 << num << " ";
         }
     }
 
-    // Close files
     fin.close();
     fout2.close();
+    cout << endl;
 
-    // Open second file for reading and print contents to console
+
     ifstream fin2("data2.txt");
-    cout << "Numbers divisible by " << m << ": ";
+    cout << "data2.txt: ";
     while (fin2 >> num) {
         cout << num << " ";
     }
     cout << endl;
 
-    // Close second file
     fin2.close();
 
     return 0;
